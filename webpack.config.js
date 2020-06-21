1ceddf7ba02ac5e6ab918b228de732f9a6a59904
isaac <aderonmuisaac@gmail.com>
2020-06-21 18:12:26 +0100

    chore: add babel-loader rule to js files

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,14 @@ const path = require("path")
 const webpack = require("webpack")
 module.exports = {
     entry: './src/client/index.js',
-    mode: 'development'
+    mode: 'development',
+    module: {
+        rules: [
+                {
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+                }
+        ]
+    }
 }
